903847dd7760b525e1e49810ba5826c7c4864077
Jose Herrera <jherrera892016@gmail.com>
2022-11-08 13:02:52 -0500

    add(core): Add Function ProfileScreen

--- src/Screens/ProfileScreen.jsx
+++ src/Screens/ProfileScreen.jsx
@@ -1,37 +1,76 @@
-import { Button, Platform, StyleSheet, Text, View , PermissionsAndroid, Image} from 'react-native'
+import { Button, Platform, StyleSheet, Text, View , PermissionsAndroid, Image, TouchableOpacity} from 'react-native'
 import React,{useState} from 'react'
 import {launchCamera,launchImageLibrary} from 'react-native-image-picker'
 
 const ProfileScreen = () => {
 const [cameraPhoto, setCameraPhoto] = useState()
-const [galleryPhoto, setGalleryPhoto] = useState()  
-  let options = {
+const [galleryPhoto, setGalleryPhoto] = useState('https://via.placeholder.com/200')  
+
+const selectImage = async () => {
+  const options = {
+    title: 'Selecciona una Imagen',
+    storageOptions: {
+      skipBackup: true,
+      path: 'images',
+    },
     saveToPhotos: true,
     mediaType: 'photo',
+    
   }
 
-  const openCamera = async () => {
-      const granted = await PermissionsAndroid.request(
-        PermissionsAndroid.PERMISSIONS.CAMERA,
-        );
-      if(granted === PermissionsAndroid.RESULTS.GRANTED) {
-        const result = await launchCamera(options);
-        setCameraPhoto(result.assets[0].uri)
-      }
+  await launchImageLibrary(options,response =>{
+    if(response.errorCode){
+      alert(response.errorMessage)
+    }else if (response.didCancel){
+      alert('Usuario Cancelo Option!!!')
+    } else {
+      const path =response.assets[0].uri
+      setGalleryPhoto(path)
+    }
+  })}
+
+
+  const takePicture = async () => {
+    const options = {
+      title: 'Tomar una Imagen',
+      storageOptions: {
+        skipBackup: true,
+        path: 'images',
+      },
+      
+      presentationStyle: 'popover',
+      includeBase64: true,
+      saveToPhotos: true,
+      mediaType: 'photo',
+    }
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.CAMERA,
+    );
+    if(granted === PermissionsAndroid.RESULTS.GRANTED){
+      await launchCamera(options, response => {
+        if(response.errorCode){
+          alert(response.errorMessage)
+        }else if (response.didCancel){
+          alert('Usuario Cancelo Option!!!')
+        } else {
+          const path =response.assets[0].uri
+          setCameraPhoto(path)
+        }
+      })
+    }
+
   }
 
-  const openGallery = async () => {
-    const result = await launchImageLibrary(options)
-    setGalleryPhoto(result.assets[0].uri)
-  }
   return (
     <>
       <Text>ProfileScreen</Text>
-      <Button title='Open Image Library' onPress={openCamera}/>
+      <Button title='Capture Photo' onPress={takePicture}/>
       <Image style={{width:200 , height:200, borderRadius:15}} source={{uri:cameraPhoto}}/>
-      <Button title='Open Gallery' onPress={openGallery}/>
+      {/* <Button title='Open Gallery' /> */}
       <Text>Gallery Photo</Text>
+      <TouchableOpacity onPress={selectImage}>
       <Image style={{width:200 , height:200, borderRadius:15}} source={{uri:galleryPhoto}}/>
+      </TouchableOpacity>
     </>
   )
 }
